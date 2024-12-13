9ac8f3002b1e0e27f1ccfa6e44174c053adcd801
Gabriel Rufino <contato@gabrielrufino.com>
2024-12-13 13:40:24 -0300

    chore

--- src/search/test_search.rs
+++ src/search/test_search.rs
@@ -1,6 +1,8 @@
 #[macro_export]
 macro_rules! test_search {
   ($search_fn: expr) => {
+    // use std::time::Instant;
+
     #[test]
     fn test_element_found() {
       let numbers = vec![1, 3, 5, 7, 9];
@@ -42,5 +44,19 @@ macro_rules! test_search {
       let numbers = vec![42];
       assert_eq!($search_fn(&numbers, 24), None);
     }
+
+    /*
+    #[test]
+    fn test_performance() {
+      let size: u64 = 1_000_000_000;
+      let numbers: Vec<u64> = (1..=size).collect();
+      let target = size;
+    
+      let start = Instant::now();
+      let result = $search_fn(&numbers, target);
+      let duration = start.elapsed();
+      println!("Search: {:?}, Duration: {:?}", result, duration);
+    }
+    */
   };
 }
