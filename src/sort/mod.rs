64423647f2d944d34b1d6019ae04c9041c2b2b88
Gabriel Rufino <contato@gabrielrufino.com>
2024-12-15 22:47:36 -0300

    refactor: tests

--- src/sort/mod.rs
+++ src/sort/mod.rs
@@ -1,1 +1,4 @@
+#[macro_use]
+mod test_sort;
+
 pub mod selection_sort;
--- src/sort/selection_sort.rs
+++ src/sort/selection_sort.rs
@@ -20,45 +20,5 @@ pub fn selection_sort<T: Ord>(arr: &mut [T]) {
 mod tests {
   use super::*;
 
-  #[test]
-  fn test_empty_array() {
-    let mut arr: Vec<i32> = vec![];
-    selection_sort(&mut arr);
-    assert_eq!(arr, vec![]);
-  }
-
-  #[test]
-  fn test_single_element() {
-    let mut arr = vec![1];
-    selection_sort(&mut arr);
-    assert_eq!(arr, vec![1]);
-  }
-
-  #[test]
-  fn test_sorted_array() {
-    let mut arr = vec![1, 2, 3, 4, 5];
-    selection_sort(&mut arr);
-    assert_eq!(arr, vec![1, 2, 3, 4, 5]);
-  }
-
-  #[test]
-  fn test_reverse_sorted_array() {
-    let mut arr = vec![5, 4, 3, 2, 1];
-    selection_sort(&mut arr);
-    assert_eq!(arr, vec![1, 2, 3, 4, 5]);
-  }
-
-  #[test]
-  fn test_unsorted_array() {
-    let mut arr = vec![64, 25, 12, 22, 11];
-    selection_sort(&mut arr);
-    assert_eq!(arr, vec![11, 12, 22, 25, 64]);
-  }
-
-  #[test]
-  fn test_duplicates() {
-    let mut arr = vec![3, 3, 2, 1, 1];
-    selection_sort(&mut arr);
-    assert_eq!(arr, vec![1, 1, 2, 3, 3]);
-  }
+  test_sort!(selection_sort);
 }
