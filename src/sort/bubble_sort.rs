96642c555d35b6b3b6e9c70b65d91801a83d7356
Gabriel Rufino <contato@gabrielrufino.com>
2026-03-16 19:42:34 -0300

    refactor(sort): refactor bubble sort to struct pattern and enable tests
    - Replaces bubble_sort function with BubbleSort struct
    - Implements new and execute methods to align with project patterns
    - Uncomments and updates unit tests using the test_sort! macro

--- src/sort/bubble_sort.rs
+++ src/sort/bubble_sort.rs
@@ -1,25 +1,35 @@
-pub fn bubble_sort<T: PartialOrd>(arr: &mut [T]) {
-  let length = arr.len();
+pub struct BubbleSort<'a, T: PartialOrd> {
+  arr: &'a mut [T],
+}
 
-  for i in 0..length {
-    let mut swapped = false;
+impl<'a, T: PartialOrd> BubbleSort<'a, T> {
+  pub fn new(arr: &mut [T]) -> BubbleSort<T> {
+    BubbleSort { arr }
+  }
 
-    for j in 0..length - i - 1 {
-      if arr[j] > arr[j + 1] {
-        arr.swap(j, j + 1);
-        swapped = true
+  pub fn execute(&mut self) {
+    let length = self.arr.len();
+
+    for i in 0..length {
+      let mut swapped = false;
+
+      for j in 0..length - i - 1 {
+        if self.arr[j] > self.arr[j + 1] {
+          self.arr.swap(j, j + 1);
+          swapped = true
+        }
       }
-    }
 
-    if !swapped {
-      break;
+      if !swapped {
+        break;
+      }
     }
   }
 }
 
-// #[cfg(test)]
-// mod tests {
-//   use super::*;
-// 
-//   test_sort!(bubble_sort);
-// }
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  test_sort!(BubbleSort);
+}
